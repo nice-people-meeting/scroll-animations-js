7d1f31004076b1c5901d346a834afa28df532a19
jwun95 <jwun95@naver.com>
2022-04-17 00:58:24 +0900

    type script

---
+++ src/ts/index.ts
@@ -0,0 +1,44 @@
+let options = {
+  delay: 500,
+  duration: 500,
+};
+
+module.exports = function init(settings) {
+  options = Object.assign(options, settings);
+
+  /*
+   * IntersectionObserver if not supported
+   */
+  if (
+    !('IntersectionObserver' in window) ||
+    !('IntersectionObserverEntry' in window) ||
+    !('intersectionRatio' in window.IntersectionObserverEntry.prototype) ||
+    !('isIntersecting' in window.IntersectionObserverEntry.prototype)
+  ) {
+    console.warn('IntersectionObserver is not supported by your browser.');
+    return;
+  }
+
+  document.querySelector('body').setAttribute('sa-duration', options.duration.toString());
+  document.querySelector('body').setAttribute('sa-delay', options.delay.toString());
+
+  const io = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('sa-animated');
+          observer.unobserve(entry.target);
+        } else {
+          entry.target.classList.remove('sa-animated');
+        }
+      });
+    },
+    {
+      threshold: 0,
+      root: null,
+      rootMargin: '0px',
+    }
+  );
+
+  document.querySelectorAll('.sa-animation').forEach(element => io.observe(element));
+};
